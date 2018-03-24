a4eb29106ea7cd4f72745b39acc93c9abf93fd49
HomoRiron <okutsu1108@gmail.com>
2018-03-24 18:46:10 +0900

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord.Net
+{
+    public class DiscordLoginClient
+    {
+        public async Task<string> LoginWithEmailAndPasswordAsync(string Email, string Password)
+        {
+            var url = "https://discordapp.com/api/v6/auth/login";
+            var client = new HttpClient();
+            var reqm = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(url),
+            };
+            reqm.Content = new StringContent(
+
+                JsonConvert.SerializeObject(new
+                {
+                    email = Email,
+                    password = Password
+                }),
+                Encoding.UTF8,
+                "application/json");
+            var response = await client.SendAsync(reqm);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseDic = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseString);
+            if (responseDic.ContainsKey("captcha_key"))
+            {
+                return "Invalid Email";
+            }
+            if (responseDic.ContainsKey("password"))
+            {
+                return "Password does not match";
+            }
+            return responseDic["token"];
+        }
+
+    }
+}
